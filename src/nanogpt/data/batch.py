22c7b59ca40833ffb72e991435f89e173a3014c7
urlicht <urlicht@users.noreply.github.com>
2026-02-05 13:37:26 +0000

    change variable  names for consistency

--- src/nanogpt/data/batch.py
+++ src/nanogpt/data/batch.py
@@ -5,8 +5,8 @@ import numpy as np
 def get_batch(
     data_dir: str, 
     split: str, 
-    batch_size: int, 
-    block_size: int, 
+    n_batch: int, 
+    n_block: int, 
     device: torch.device=torch.device('cpu'), 
     dtype: type=np.uint16
 ):
@@ -15,13 +15,13 @@ def get_batch(
     data = np.memmap(filename, dtype=dtype, mode='r')
     
     # randomly sample starting indices for the batch
-    # We use len(data) - block_size - 1 to ensure we have room for the y-offset
-    ix = torch.randint(len(data) - block_size - 1, (batch_size,))
+    # We use len(data) - n_block - 1 to ensure we have room for the y-offset
+    ix = torch.randint(len(data) - n_block - 1, (n_batch,))
     
     # extract slices and stack them
     # torch requires int64
-    x = torch.stack([torch.from_numpy((data[i : i + block_size]).astype(np.int64)) for i in ix])
-    y = torch.stack([torch.from_numpy((data[i + 1 : i + 1 + block_size]).astype(np.int64)) for i in ix])
+    x = torch.stack([torch.from_numpy((data[i : i + n_block]).astype(np.int64)) for i in ix])
+    y = torch.stack([torch.from_numpy((data[i + 1 : i + 1 + n_block]).astype(np.int64)) for i in ix])
     
     # CUDA optimization
     if device.type == 'cuda':
